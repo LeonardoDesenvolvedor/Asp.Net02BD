c591c5b949e290cedf97e4af2bd47218241b9ab4
Leonardo Augusto <leonardoaugusto43@hotmail.com>
2021-05-29 21:45:54 -0300

    Classe  Conexao e Pagina de teste

---
+++ Asp .NetBD02/Asp .NetBD02/Conexao.cs
@@ -0,0 +1,54 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.NetBD02
+{
+    public class Conexao
+    {
+
+        #region Parametro da Conexao
+
+        private static string Server = "localhost";
+        private static string Database = "web_cliente";
+        private static string User = "root";
+        private static string Password = "00";
+        private static string connectionStrign = $@"Server={Server};Database={Database};
+                                                     Uid={User};Pwd={Password};SslMode=none;
+                                                     Charset=utf8;";
+
+        #endregion
+
+
+        // Trazer a biblioteca do MySql Connector
+
+        public static MySqlConnection Connection = new MySqlConnection(connectionStrign);
+
+
+
+        #region Metodo Conectar
+        public static void Conectar()
+        {
+            if (Connection.State == System.Data.ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+        }
+        #endregion
+
+        #region Metodo Desconectar
+
+        public static void Desconectar()
+        {
+            if (Connection.State == System.Data.ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Asp .NetBD02/Asp .NetBD02/TesteBD.aspx.cs
@@ -0,0 +1,44 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetBD02
+{
+    public partial class TesteBD : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTestar_Click(object sender, EventArgs e)
+        {
+
+            MySqlCommand cmd = new MySqlCommand();
+
+
+            try
+            {
+                cmd.Connection = Conexao.Connection;
+
+                lblResultado.CssClass = "text text-success";
+                lblResultado.Text = "Ok... Conectado com Sucesso";
+
+            }
+            catch (Exception ex)
+            {
+                lblResultado.CssClass = "text text-danger";
+                lblResultado.Text = "Atenção: " + ex.Message;
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+
+        }
+    }
+}
